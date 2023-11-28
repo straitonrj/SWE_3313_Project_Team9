66d8cca3990595b948d1184033521bef86d45c0d
Notesboy-123 <103542864+Notesboy-123@users.noreply.github.com>
2023-11-27 22:09:06 -0500

    Add files via upload

---
+++ Implementation/Code/11-17-13 GUI Draft/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+        LoginScreen StartPage = new LoginScreen();
+        StartPage.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+
+
+    }
+}
