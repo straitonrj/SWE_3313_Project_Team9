85b4a4e4775aacb36e30be14b8ede6f2d85a8b9f
Notesboy-123 <103542864+Notesboy-123@users.noreply.github.com>
2023-11-30 17:49:21 -0500

    Add files via upload
    V.1.2.1

---
+++ Integrated Code V.1.2.1/untitled/src/main/java/org/UCars/SignUpScreen.java
@@ -0,0 +1,70 @@
+package org.UCars;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SignUpScreen extends JFrame{
+    private JPanel SignUpPanel;
+    private JTextField UsernameSignUp;
+    private JPasswordField PasswordSignUp;
+    private JButton createYourAccountButton;
+    private JButton LoginSwitchButton;
+    private JTextField textField1;
+    private JLabel UCarsLogoPicture;
+    private JPanel SignUpScreen;
+    private static String Error;
+    private JLabel SignUpErrorMessage;
+
+
+    public SignUpScreen(){
+
+        //Sets up the page, makes it visible, and creates the ability to close the program
+        setContentPane(SignUpScreen);
+        setTitle("Ucars Application");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800,800);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        //Adds the logo onto the screen.
+        ImageIcon LogoBlue = new ImageIcon("untitled/src/main/java/org/UCars/UCarsLogoBlue.png");
+        UCarsLogoPicture.setIcon(LogoBlue);
+
+
+        //On button press, gets rid of the current Sign-up screen and provides the Login Screen .
+        LoginSwitchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LoginScreen LoginPage = new LoginScreen();
+                setVisible(false);
+                LoginPage.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            }
+        });
+
+        //On button press, create account and go to the home page
+        createYourAccountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int Temp = Main.SignUp(UsernameSignUp.getText(),PasswordSignUp.getText());
+                if(Temp == 1){
+                    SignUpErrorMessage.setText("Username Taken");
+                    SignUpErrorMessage.setVisible(true);
+                } else if (Temp == 2) {
+                    SignUpErrorMessage.setText("Password must contain a minimum of six characters.");
+                    SignUpErrorMessage.setVisible(true);
+                } else if (Temp==0) {
+                    SignUpErrorMessage.setVisible(false);
+                    Main.Login(UsernameSignUp.getText(),PasswordSignUp.getText());
+
+                    HomeScreen HomePage = new HomeScreen();
+                    setVisible(false);
+                    HomePage.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                }
+            }
+        });
+    }
+
+
+
+}
